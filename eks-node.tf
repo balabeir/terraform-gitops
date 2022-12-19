2368dfd87102b987a344955bee6914da08cad63c
wtpJ <wutthiphongj@donuts-bkk.com>
2022-12-19 22:47:13 +0700

    scaling_config

--- eks-node.tf
+++ eks-node.tf
@@ -39,10 +39,10 @@ resource "aws_eks_node_group" "private-nodes" {
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.medium"]
+  instance_types = ["t2.micro"]
 
   scaling_config {
-    desired_size = 0
+    desired_size = 1
     max_size     = 2
     min_size     = 0
   }
